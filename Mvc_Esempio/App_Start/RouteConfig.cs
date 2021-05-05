d23282850080c93bdc1588153ca6d8c4e7185016
FraCata00 <78490028+FraCata00@users.noreply.github.com>
2021-05-05 14:29:19 +0200

    fix
    fix

---
+++ Mvc_Esempio/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mvc_Esempio
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "InserisciProdotto",
+                url: "Prodotti/InserisciProdotto/{prodotto}",
+                defaults: new { controller = "Prodotti", action = "InserisciProdotto", prodotto = UrlParameter.Optional }
+            );
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Mvc_Esempio/Controllers/ProdottiController.cs
@@ -0,0 +1,85 @@
+﻿using Mvc_Esempio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Esempio.Controllers
+{
+    public class ProdottiController : Controller
+    {
+
+        //accesso al database
+        private Cloud_MyShopEntities db = new Cloud_MyShopEntities();
+
+        // GET: Prodotti
+        public ActionResult Index()
+        {
+            //elenco dei prodotti 
+            var elenco = db.Prodotto.ToList();
+            return View(elenco);
+        }
+
+        //GET: Prodotti/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Prodotti/Create
+        public ActionResult InserisciProdotto(Prodotto prodotto)
+        {
+            if(!ModelState.IsValid)
+                return HttpNotFound();
+
+            if(prodotto == null)
+                return RedirectToAction("Errore");
+
+            //esito positivo
+            db.Prodotto.Add(prodotto);
+            db.SaveChanges();
+            return RedirectToAction("PaginaOk");
+            
+            //esito negativo
+        }
+
+        //GET: Prodotti/Delete
+        public ActionResult Delete(int id)
+        {
+            var prodotto = db.Prodotto.Find(id);
+
+            if (prodotto == null)
+                return RedirectToAction("Errore");
+
+            return View(prodotto);
+        }
+
+        //POST:Prodotti/Delete
+        public ActionResult EliminaProdotto(int id)
+        {
+            var prodotto = db.Prodotto.Find(id);
+
+            if (prodotto == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            db.Prodotto.Remove(prodotto);
+            db.SaveChanges();
+
+            return RedirectToAction("Index"); //ritorno all'elenco dei prodotti
+        }
+
+        public ActionResult PaginaOk()
+        {
+            ViewBag.Messaggio = "Operazione eseguita con successo!";
+            return View();
+        }
+
+        public ActionResult Errore()
+        {
+            ViewBag.Messaggio = "Operazione fallita!";
+            return View();
+        }
+    }
+}
